710b5527c932811bb22094572f84dcf12929c6d4
Asad Ali <14asadali@gmail.com>
2025-10-27 10:48:14 +0500

    updated client.ts file template

--- src/client.ts
+++ src/client.ts
@@ -1,15 +1,9 @@
-import { GraphQLClient, QueryType, GraphQLResult } from './core/index.js';
-<$rootSchemaImports>
-<$responsesImports>
-<$inputsImports>
-<$queriesImports>
+import { GraphQLClient, QueryType, GraphQLResult } from './core/index.js';<$rootSchemaImports><$responsesImports><$inputsImports><$queriesImports>
 
 export class Client {
   private client: GraphQLClient;
 
   constructor() {
     this.client = new GraphQLClient(<$clientConfig>);
-  }
-
-  <$clientMethods>
+  }<$clientMethods>
 }
