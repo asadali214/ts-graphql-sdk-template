2ab64b42afedb4d008ff94787f7d383ad6383436
Asad Ali <14asadali@gmail.com>
2025-10-28 12:19:26 +0500

    add a new placeholder for root level enum imports

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,4 @@
-import { GraphQLClient, QueryType, GraphQLResult } from './core/index.js';<$rootSchemaImports><$responsesImports><$inputsImports><$queriesImports>
+import { GraphQLClient, QueryType, GraphQLResult } from './core/index.js';<$rootSchemaImports><$rootEnumImports><$responsesImports><$inputsImports><$queriesImports>
 
 export class Client {
   private client: GraphQLClient;
